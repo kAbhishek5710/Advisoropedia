95e33909e1daebff146f7c5a31522ab8797406d4
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-03-06 18:36:12 +0530

    Complete signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json()); //by default json is not allowed to send to server directly
+
 app.listen(8080, () => {
   console.log("App is listening to port 8080");
 });
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);

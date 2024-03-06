ded36b41f79fc61eaab9de5727ae6063946f53a7
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-03-06 17:49:19 +0530

    Complete Header Section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      Header
+      <header className="bg-slate-200 shadow-md">
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+          <Link to="/">
+            <h1 className="font-bold text-sm sm:text-xl text-slate-700 uppercase">
+              Advisorpedia
+            </h1>
+          </Link>
+          <form className="flex bg-slate-100 p-3 rounded-lg items-center">
+            <input
+              className="bg-transparent focus:outline-none w-24 sm:w-64"
+              type="text"
+              placeholder="Search..."
+            />
+            <FaSearch className="text-slate-600" />
+          </form>
+          <ul className="flex gap-4">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="#">
+              <li>About</li>
+            </Link>
+            <Link to="/sign-in">
+              <li>Sign In</li>
+            </Link>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }

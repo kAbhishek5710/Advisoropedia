7adea76c1bd5c01777fa1ad3cfbc3fa4ca63ddf9
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-03-07 20:41:09 +0530

    Create user upadate api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,6 +18,8 @@ mongoose
 const app = express();
 
 app.use(express.json()); //by default json is not allowed to send to server directly
+
+app.use(cookieParser());
 
 app.listen(8080, () => {
   console.log("App is listening to port 8080");
